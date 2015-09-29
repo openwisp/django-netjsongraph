08cf3c7ee33698e67150d218d13582432649b60b
Federico Capoano <nemesis@ninux.org>
2015-09-29 18:57:38 +0200

    Added models

---
+++ django_netjsongraph/models/__init__.py
@@ -0,0 +1,18 @@
+from django.utils.translation import ugettext_lazy as _
+
+from .topology import BaseTopology
+from .node import BaseNode
+from .link import BaseLink
+
+
+class Topology(BaseTopology):
+    class Meta(BaseTopology.Meta):
+        abstract = False
+
+
+class Node(BaseNode):
+    pass
+
+
+class Link(BaseLink):
+    pass
