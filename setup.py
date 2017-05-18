455048d068577d94a3668f11b4c86251e92fd08e
Federico Capoano <nemesis@ninux.org>
2017-05-18 12:28:59 +0200

    [packaging] Improved setup.py and MANIFEST.in
    Exclude unneeded files from resulting package.

--- setup.py
+++ setup.py
@@ -23,7 +23,9 @@ def get_install_requires():
 if sys.argv[-1] == 'publish':
     # delete any *.pyc, *.pyo and __pycache__
     os.system('find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf')
-    os.system("python setup.py sdist bdist_wheel upload -s")
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload -s dist/*")
+    os.system("rm -rf dist build")
     args = {'version': get_version()}
     print("You probably want to also tag the version now:")
     print("  git tag -a %(version)s -m 'version %(version)s'" % args)
@@ -42,7 +44,7 @@ setup(
     download_url='https://github.com/interop-dev/django-netjsongraph/releases',
     platforms=['Platform Indipendent'],
     keywords=['django', 'netjson', 'mesh', 'networking'],
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     install_requires=get_install_requires(),
     include_package_data=True,
     zip_safe=False,
