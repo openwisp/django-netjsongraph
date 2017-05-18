a5a55e34f01411286de31ae7710385324179a0f3
Federico Capoano <nemesis@ninux.org>
2017-05-18 12:00:26 +0200

    [JS] Refactored receive-url.js
    It should be more readable now.

--- django_netjsongraph/static/netjsongraph/receive-url.js
+++ django_netjsongraph/static/netjsongraph/receive-url.js
@@ -1,9 +1,11 @@
-django.jQuery(function($) {
-    var p = $('.field-receive_url p, .field-receive_url > div > div'),
-        value = window.location.origin + p.text();
-    p.html('<input readonly id="id_receive_url" type="text" class="vTextField readonly" value="'+ value +'">');
-    var field = $('#id_receive_url');
-    field.click(function(){
-        $(this).select();
+(function($) {
+    $(document).ready(function() {
+        var p = $('.field-receive_url p, .field-receive_url > div > div'),
+            value = window.location.origin + p.text();
+        p.html('<input readonly id="id_receive_url" type="text" class="vTextField readonly" value="'+ value +'">');
+        var field = $('#id_receive_url');
+        field.click(function(){
+            $(this).select();
+        });
     });
-});
+})(django.jQuery);
