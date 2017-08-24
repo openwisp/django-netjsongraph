dafb1d39362bbf03c46976fa6849d5c25d19f21a
Federico Capoano <nemesis@ninux.org>
2017-08-24 16:30:05 +0200

    [history] Simplified topology history JS logic

--- django_netjsongraph/static/netjsongraph/js/topology-switcher.js
+++ django_netjsongraph/static/netjsongraph/js/topology-switcher.js
@@ -1,27 +1,17 @@
 window.initTopologyHistory = function($){
     var datepicker = $('#njg-datepicker'),
-        d = new Date(),
-        month = d.getMonth() + 1,
-        day = d.getDate(),
-        defaultDate = (month < 10 ? '0' : '') + month + '/' +
-                      (day < 10 ? '0' : '') + day + '/' + d.getFullYear(),
-        currentDate = d.getFullYear() + '-' + (month < 10 ? '0' : '') +
-                      month + '-' + (day < 10 ? '0' : '') + day;
-    datepicker.datepicker();
-    datepicker.val(defaultDate);
-    datepicker.change(function() {
-        var queryDate = datepicker.val(),
-            date = queryDate.split('/').reverse(),
-            graphUrl;
-        if(date != []){
-            var x = date[1];
-            date[1] = date[2];
-            date[2] = x;
-        }
-        date = date.join('-');
-        graphUrl = datepicker.attr('data-history-api') + '?date=' + date;
+        today = new Date(),
+        apiUrl = datepicker.attr('data-history-api');
+    today.setHours(0, 0, 0, 0);
+    datepicker.datepicker({dateFormat: 'dd/mm/yy'});
+    datepicker.datepicker('setDate', today);
+    datepicker.change(function() {;
+        var date = datepicker.val().split('/').reverse().join('-'),
+            url = apiUrl + '?date=' + date;
         // load latest data when looking currentDate
-        if(currentDate == date){ graphUrl = undefined }
-        window.graph = window.loadNetJsonGraph(graphUrl);
+        if(datepicker.datepicker('getDate').getTime() == today.getTime()){
+            url = undefined
+        }
+        window.graph = window.loadNetJsonGraph(url);
     });
 };
