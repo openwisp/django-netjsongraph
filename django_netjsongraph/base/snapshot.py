249b3a6c98a8b2bc95283e14a0fdeeed76162e3b
Leonardo Maccari <leonardo.maccari@unitn.it>
2017-11-20 16:40:32 +0100

    added kind field to snapshots

--- django_netjsongraph/base/snapshot.py
+++ django_netjsongraph/base/snapshot.py
@@ -3,6 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 
 from .base import TimeStampedEditableModel
 
+KINDS = (
+    ('normal', _('NORMAL')),
+    ('block_cut_tree', _('BLOCK_CUT_TREE'))
+)
+
 
 class AbstractSnapshot(TimeStampedEditableModel):
     """
@@ -11,6 +16,11 @@ class AbstractSnapshot(TimeStampedEditableModel):
     topology = models.ForeignKey('django_netjsongraph.topology')
     data = models.TextField(blank=False)
     date = models.DateField(auto_now=True)
+    kind = models.CharField(_('kind'),
+                            max_length=16,
+                            choices=KINDS,
+                            default='normal',
+                            db_index=True)
 
     class Meta:
         verbose_name_plural = _('snapshots')
